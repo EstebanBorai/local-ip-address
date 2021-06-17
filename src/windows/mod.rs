81a8bed83d37b500279785dfabd2a19981e55b95
nicguzzo <nicguzzo@gmail.com>
2021-06-17 14:49:03 -0300

    split code into modules for unixes and windows

---
+++ src/windows/mod.rs
@@ -0,0 +1,7 @@
+pub fn find_af_inet<'a>() -> Result<Vec<(&'a str, IpAddr)>, Error> {
+    //TODO: implement this on windows
+}
+
+pub fn local_ip() -> Result<IpAddr, Error> {
+    //TODO: implement this on windows
+}
