40a6f6a86b5e7dd5c7b81ecbe4684a307d60e9d3
Christian Göttsche <cgzones@googlemail.com>
2022-11-16 19:24:16 +0100

    examples: add simple example executable

---
+++ examples/show_ip_and_ifs.rs
@@ -0,0 +1,18 @@
+use local_ip_address::{list_afinet_netifas, local_ip};
+
+fn main() {
+    match local_ip() {
+        Ok(ip) => println!("Local IP: {ip}"),
+        Err(err) => println!("Failed to get local IP: {err}"),
+    };
+
+    match list_afinet_netifas() {
+        Ok(netifs) => {
+            println!("Got {} interfaces", netifs.len());
+            for netif in netifs {
+                println!("IF: {}, IP: {}", netif.0, netif.1);
+            }
+        }
+        Err(err) => println!("Failed to get list of network interfaces: {err}"),
+    };
+}
