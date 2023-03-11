2fe3acdddc9ba60db2c0fc492f2de5dc7cdfd0f9
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-03-11 12:42:36 +0800

    add some unit tests on parse_ifname function

--- src/linux.rs
+++ src/linux.rs
@@ -365,14 +365,53 @@ pub fn list_afinet_netifas() -> Result<Vec<(String, IpAddr)>, Error> {
     Ok(interfaces)
 }
 
+/// Parse network interface name of slice type to string type.
+/// If the slice is suffixed with '\0', this suffix will be removed when parsing.
 fn parse_ifname(bytes: &[u8]) -> Result<String, Error> {
-    let ifname = CStr::from_bytes_with_nul(bytes)
-        .map_err(|_| {
-            Error::StrategyError(String::from(
-                "An error occurred convert interface name to string",
-            ))
-        })?
-        .to_string_lossy()
-        .to_string();
+    let ifname = if bytes.ends_with(&[0u8]) {
+        CStr::from_bytes_with_nul(bytes)
+            .map_err(|err| {
+                Error::StrategyError(format!(
+                    "An error occurred converting interface name to string: {err}",
+                ))
+            })?
+            .to_string_lossy()
+            .to_string()
+    } else {
+        String::from_utf8_lossy(bytes).to_string()
+    };
+
     Ok(ifname)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::linux::parse_ifname;
+
+    #[test]
+    fn parse_ifname_without_nul() {
+        let expected = "hello, world";
+        let bytes = [104, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100];
+        let res = parse_ifname(&bytes);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), expected);
+    }
+
+    #[test]
+    fn parse_ifname_with_nul() {
+        let expected = "hello, world";
+        let bytes = [104, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 0];
+        let res = parse_ifname(&bytes);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), expected);
+    }
+
+    #[test]
+    fn parse_ifname_only_nul() {
+        let expected = "";
+        let bytes = [0u8];
+        let res = parse_ifname(&bytes);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), expected);
+    }
 }
